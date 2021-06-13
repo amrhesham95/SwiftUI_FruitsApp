28af5d6bf324f33475143f6c5ed2592f66e0c5e9
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-06-13 15:20:41 +0200

    initial commit
    creating the project

---
+++ FruitsApp/Shared/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  Shared
+//
+//  Created by Amr Hesham on 13/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
